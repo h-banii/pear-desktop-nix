a15c6643fdb957dd6959f17f0b08d9ff1b0f76eb
h-banii <121690516+h-banii@users.noreply.github.com>
2025-09-07 20:50:50 -0300

    hm-module: Bump default version to 3.11.0

--- nix/hm-module/default.nix
+++ nix/hm-module/default.nix
@@ -23,7 +23,7 @@ in
     url = mkOption { default = "https://music.youtube.com"; };
     version = mkOption {
       description = "Version used in migrations";
-      default = "3.10.0";
+      default = "3.11.0";
     };
     configFolderName = mkOption {
       description = "Name of the config folder";
