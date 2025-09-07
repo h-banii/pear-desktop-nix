6cdac81508a7b713d81194e4bb7373aa7a64b8ba
h-banii <121690516+h-banii@users.noreply.github.com>
2025-09-07 18:52:00 -0300

    hm-module/plugins/discord: Set types

--- nix/hm-module/plugins/discord.nix
+++ nix/hm-module/plugins/discord.nix
@@ -4,29 +4,27 @@ let
 in
 {
   enabled = mkEnableOption "Discord Rich Presence plugin";
-  autoReconnect = mkOption {
+  autoReconnect = mkEnableOption "" // {
     description = "If enabled, will try to reconnect to discord every 5 seconds after disconnecting or failing to connect";
     default = true;
   };
-  activityTimeoutEnabled = mkOption {
+  activityTimeoutEnabled = mkEnableOption "" // {
     description = "If enabled, the discord rich presence gets cleared when music paused after the time specified below";
     default = true;
   };
   activityTimeoutTime = mkOption {
     description = "The time in milliseconds after which the discord rich presence gets cleared when music paused";
     default = 10 * 60 * 1000;
+    type = types.number;
   };
-  playOnYouTubeMusic = mkOption {
+  playOnYouTubeMusic = mkEnableOption "" // {
     description = ''Add a "Play on YouTube Music" button to rich presence'';
-    default = false;
   };
-  hideGitHubButton = mkOption {
+  hideGitHubButton = mkEnableOption "" // {
     description = ''Hide the "View App On GitHub" button in the rich presence'';
-    default = false;
   };
-  hideDurationLeft = mkOption {
+  hideDurationLeft = mkEnableOption "" // {
     description = ''Hide the "duration left" in the rich presence'';
-    default = false;
   };
   statusDisplayType = mkOption {
     description = ''
