804c78f8c24a4093a1cd1f8f56b2b3d3f055a2be
h-banii <121690516+h-banii@users.noreply.github.com>
2025-09-09 12:59:04 -0300

    hm-module/plugins: Use list of plugins to generate simple options

--- nix/hm-module/plugins/default.nix
+++ nix/hm-module/plugins/default.nix
@@ -2,63 +2,69 @@
 let
   inherit (lib) mkEnableOption mergeAttrs;
   inherit (builtins) listToAttrs map;
+
+  complexPlugins = [
+    "adblocker"
+    "album-color-theme"
+    "ambient-mode"
+    "api-server"
+    "auth-proxy-adapter"
+    "captions-selector"
+    "crossfade"
+    "custom-output-device"
+    "disable-autoplay"
+    "discord"
+    "downloader"
+    "equalizer"
+    "in-app-menu"
+    "lyrics-genius"
+    "notifications"
+    "picture-in-picture"
+    "precise-volume"
+    "scrobbler"
+    "shortcuts"
+    "skip-silences"
+    "sponsorblock"
+    "synced-lyrics"
+    "transparent-player"
+    "video-toggle"
+    "visualizer"
+  ];
+
+  simplePlugins = {
+    audio-compressor = "Audio Compressor plugin";
+    album-actions = "Album Actions plugin";
+    amuse = "Amuse plugin";
+    blur-nav-bar = "Blur Navigation Bar plugin";
+    bypass-age-restrictions = "Bypass Age Restrictions plugin";
+    compact-sidebar = "Compact Sidebar plugin";
+    exponential-volume = "Exponential Volume plugin";
+    lumiastream = "Lumia Stream plugin";
+    music-together = "Music Together plugin";
+    navigation = "Navigation plugin";
+    no-google-login = "No Google Login plugin";
+    playback-speed = "Playback Speed plugin";
+    quality-changer = "Quality Changer plugin";
+    skip-disliked-songs = "Skip Disliked Songs plugin";
+    taskbar-mediacontrol = "Taskbar Media Control plugin (Windows)";
+    touchbar = "TouchBar plugin";
+    tuna-obs = "Tuna OBS plugin";
+    unobtrusive-player = "Unobtrusive Player plugin";
+    performance-improvement = "Performance Improvement plugin";
+  };
 in
 {
   options.programs.youtube-music.plugins =
     mergeAttrs
       (listToAttrs (
-        map
-          (plugin: {
-            name = plugin;
-            value = import ./${plugin}.nix { inherit lib; };
-          })
-          [
-            "adblocker"
-            "album-color-theme"
-            "ambient-mode"
-            "api-server"
-            "auth-proxy-adapter"
-            "captions-selector"
-            "crossfade"
-            "custom-output-device"
-            "disable-autoplay"
-            "discord"
-            "downloader"
-            "equalizer"
-            "in-app-menu"
-            "lyrics-genius"
-            "notifications"
-            "picture-in-picture"
-            "precise-volume"
-            "scrobbler"
-            "shortcuts"
-            "skip-silences"
-            "sponsorblock"
-            "synced-lyrics"
-            "transparent-player"
-            "video-toggle"
-            "visualizer"
-          ]
+        map (plugin: {
+          name = plugin;
+          value = import ./${plugin}.nix { inherit lib; };
+        }) complexPlugins
       ))
-      {
-        audio-compressor.enabled = mkEnableOption "Audio Compressor plugin";
-        album-actions.enabled = mkEnableOption "Album Actions plugin";
-        amuse.enabled = mkEnableOption "Amuse plugin";
-        blur-nav-bar.enabled = mkEnableOption "Blur Navigation Bar plugin";
-        bypass-age-restrictions.enabled = mkEnableOption "Bypass Age Restrictions plugin";
-        compact-sidebar.enabled = mkEnableOption "Compact Sidebar plugin";
-        exponential-volume.enabled = mkEnableOption "Exponential Volume plugin";
-        lumiastream.enabled = mkEnableOption "Lumia Stream plugin";
-        music-together.enabled = mkEnableOption "Music Together plugin";
-        navigation.enabled = mkEnableOption "Navigation plugin";
-        no-google-login.enabled = mkEnableOption "No Google Login plugin";
-        playback-speed.enabled = mkEnableOption "Playback Speed plugin";
-        quality-changer.enabled = mkEnableOption "Quality Changer plugin";
-        skip-disliked-songs.enabled = mkEnableOption "Skip Disliked Songs plugin";
-        taskbar-mediacontrol.enabled = mkEnableOption "Taskbar Media Control plugin (Windows)";
-        touchbar.enabled = mkEnableOption "TouchBar plugin";
-        tuna-obs.enabled = mkEnableOption "Tuna OBS plugin";
-        unobtrusive-player.enabled = mkEnableOption "Unobtrusive Player plugin";
-        performance-improvement.enabled = mkEnableOption "Performance Improvement plugin";
-      };
+      (
+        lib.attrsets.mapAttrs (pluginName: pluginDescription: {
+          enabled = mkEnableOption pluginDescription;
+        }) simplePlugins
+      );
 }
