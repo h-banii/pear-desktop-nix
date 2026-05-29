a2531f46ecf5a53898227413708b57b2d332fd4c
h-banii <121690516+h-banii@users.noreply.github.com>
2026-05-29 11:29:48 -0300

    flake: Add other supported systems

--- flake.nix
+++ flake.nix
@@ -7,17 +7,24 @@
       ...
     }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      inherit (nixpkgs) lib;
+      forAllSystems = lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      pkgsFor = forAllSystems (system: nixpkgs.legacyPackages.${system});
     in
     {
       homeManagerModules.default = ./nix/hm-module;
 
       # TODO: Move this to vue-nix-manual
       lib = {
-        mkOptionsDoc = { module }: pkgs.callPackage ./nix/packages/options-doc.nix { inherit module; };
+        mkOptionsDoc =
+          { module }: pkgsFor.x86_64-linux.callPackage ./nix/packages/options-doc.nix { inherit module; };
       };
 
-      formatter.${system} = pkgs.nixfmt-tree;
+      formatter = forAllSystems (system: pkgsFor.${system}.nixfmt-tree);
     };
 }
