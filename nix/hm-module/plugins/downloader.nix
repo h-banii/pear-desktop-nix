64f25d4970c978af5334cb6943857b57b75441cc
h-banii <121690516+h-banii@users.noreply.github.com>
2026-06-15 23:30:09 +0000

    hm-module/plugins/downloader: Add missing attributes

--- nix/hm-module/plugins/downloader.nix
+++ nix/hm-module/plugins/downloader.nix
@@ -5,14 +5,24 @@ in
 {
   enable = mkEnableOption "Downloader plugin";
   downloadFolder = mkOption {
+    description = "download folder";
     default = null;
     type = types.nullOr types.str;
   };
   downloadOnFinish = {
-    enable = mkOption { default = false; };
-    seconds = mkOption { default = 20; };
-    percent = mkOption { default = 10; };
+    enable = mkEnableOption "download on finish";
+    seconds = mkOption {
+      description = "last x seconds";
+      default = 20;
+      type = types.number;
+    };
+    percent = mkOption {
+      description = "last x percent";
+      default = 10;
+      type = types.number;
+    };
     mode = mkOption {
+      description = "time mode";
       type = types.enum [
         "percent"
         "seconds"
@@ -20,12 +30,14 @@ in
       default = "seconds";
     };
     folder = mkOption {
+      description = "download on finish folder";
       default = null;
       type = types.nullOr types.str;
     };
   };
   selectedPreset = mkOption {
     default = "mp3 (256kbps)";
+    description = "downloader preset";
     type = types.enum [
       "mp3 (256kbps)"
       "Custom"
@@ -34,10 +46,12 @@ in
   };
   customPresetSetting = {
     extension = mkOption {
+      description = "custom preset setting extension";
       default = "mp3";
       type = types.nullOr types.str;
     };
     ffmpegArgs = mkOption {
+      description = "custom preset setting ffmpeg args";
       default = [
         "-b:a"
         "256k"
@@ -45,8 +59,9 @@ in
       type = types.listOf types.str;
     };
   };
-  skipExisting = mkOption { default = false; };
+  skipExisting = mkEnableOption "skip existing";
   playlistMaxItems = mkOption {
+    description = "playlist max items";
     default = null;
     type = types.nullOr types.float;
   };
