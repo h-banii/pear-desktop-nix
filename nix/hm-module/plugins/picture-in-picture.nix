6ead37a8cf11f2a4f195488ce15b033b02a388af
h-banii <121690516+h-banii@users.noreply.github.com>
2026-06-15 22:19:04 +0000

    hm-module/plugins/picture-in-picture: Add descriptions and set internal

--- nix/hm-module/plugins/picture-in-picture.nix
+++ nix/hm-module/plugins/picture-in-picture.nix
@@ -4,43 +4,43 @@ let
 in
 {
   enable = mkEnableOption "Picture-in-picture plugin";
-  alwaysOnTop = mkOption {
+  alwaysOnTop = mkEnableOption "always on top" // {
     default = true;
   };
-  savePosition = mkOption {
+  savePosition = mkEnableOption "save window position" // {
     default = true;
   };
-  saveSize = mkOption {
-    default = false;
-  };
+  saveSize = mkEnableOption "save window size";
   hotkey = mkOption {
     default = "P";
     type = types.str;
+    description = "hotkey to open the PiP";
   };
-  pip-position =
-    with types;
-    mkOption {
-      default = [
-        10
-        10
-      ];
-      description = "[x y]";
-      type = listOf int;
-    };
-  pip-size =
-    with types;
-    mkOption {
-      default = [
-        450
-        275
-      ];
-      description = "[width height]";
-      type = listOf int;
-    };
+  pip-position = mkOption {
+    default = [
+      10
+      10
+    ];
+    description = "[x y]";
+    type = with types; listOf int;
+    internal = true;
+  };
+  pip-size = mkOption {
+    default = [
+      450
+      275
+    ];
+    description = "[width height]";
+    type = with types; listOf int;
+    internal = true;
+  };
   isInPiP = mkOption {
     default = false;
+    type = types.bool;
+    internal = true;
   };
   useNativePiP = mkOption {
     default = true;
+    description = "Use the browser's native PiP";
   };
 }
