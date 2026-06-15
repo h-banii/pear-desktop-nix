abfb3f53ed2cef147015777b3e6fd238c0506783
h-banii <121690516+h-banii@users.noreply.github.com>
2026-06-15 23:34:54 +0000

    hm-module/plugins/auth-proxy-adapter: Add missing attributes

--- nix/hm-module/plugins/auth-proxy-adapter.nix
+++ nix/hm-module/plugins/auth-proxy-adapter.nix
@@ -7,9 +7,11 @@ in
   hostname = mkOption {
     default = "127.0.0.1";
     type = types.str;
+    description = "auth proxy host";
   };
   port = mkOption {
     default = 4545;
     type = types.port;
+    description = "auth proxy port";
   };
 }
