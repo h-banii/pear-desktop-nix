0b65bd42e071f5e1b3597849a0ec43ba2e30c912
h-banii <121690516+h-banii@users.noreply.github.com>
2026-06-15 22:22:28 +0000

    hm-module/plugins/album-color-theme: Add ratio description

--- nix/hm-module/plugins/album-color-theme.nix
+++ nix/hm-module/plugins/album-color-theme.nix
@@ -1,8 +1,12 @@
 { lib }:
 let
-  inherit (lib) mkEnableOption mkOption;
+  inherit (lib) mkEnableOption mkOption types;
 in
 {
   enable = mkEnableOption "Album Color Theme plugin";
-  ratio = mkOption { default = 0.5; };
+  ratio = mkOption {
+    default = 0.5;
+    description = "Color mix ratio";
+    type = types.number;
+  };
 }
